c99e299fd64825c08e2a532223e37d093e2c9585
tkddn5623 <tkddn5623@g.skku.edu>
2022-11-24 00:07:17 +0900

    Developing Search

--- src/components/Fooddb.jsx
+++ src/components/Fooddb.jsx
@@ -1,9 +1,21 @@
+import {
+    Paper,
+    List,
+} from "@mui/material";
 import PropTypes from "prop-types";
-import styled from "@emotion/styled"
 import React, { useState } from "react";
 import foodname from "../assets/fooddb_name.json"
 import fooddata from "../assets/fooddb_data.json"
 
+const getFoodInfo = searchword => {
+    console.log(searchword);
+    const searchresult = fooddata['records'].filter(food => food.식품명 === searchword)
+    if (searchresult.length == 0) throw new Error("음식을 찾지 못했습니다")
+    const range = ['식품명', '식품중량', '에너지(kcal)', '탄수화물(g)', '단백질(g)', '지방(g)']
+    let output = []
+    range.forEach(info => output[info] = searchresult[0][info])
+    return output
+}
 const SearchBar = ({ search, onChange }) => {
     return (
         <div>
@@ -17,21 +29,26 @@ const SearchBar = ({ search, onChange }) => {
 }
 const Fooddb = () => {
     const [search, setSearch] = useState("");
-    const onChange = (e) => {
+    const [selected, setSelected] = useState("");
+    const onChange = e => {
         setSearch(e.target.value);
     }
-
-    const searchedFood = foodname.filter(n => n.includes(search));
-    const searchedFoodList = searchedFood.map(n => {
-        return <li>{n}</li>;
-    })
+    const onSelected = e => {
+        setSelected(e.target.value);
+        console.log(getFoodInfo(e.target.value));
+        alert(e.target.value + " 의 칼로리는 " + getFoodInfo(e.target.value)['에너지(kcal)'])
+    }
+    const range = ['식품명', '식품중량', '에너지(kcal)', '탄수화물(g)', '단백질(g)', '지방(g)']
+    const searchedFoods = foodname.filter(n => n.includes(search));
+    const selectedFood = selected;
     return (
         <>
             <SearchBar value={search} onChange={onChange} />
-            <box>{
-                searchedFoodList
-            }
-            </box>
+            <Paper style={{maxHeight: '400px', maxWidth: '300px', overflow: 'auto'}}>
+                <List>
+                    {searchedFoods.map(n => <li><button value={n} onClick={onSelected}>{n}</button></li>)}
+                </List>
+            </Paper>
 
         </>
     );
